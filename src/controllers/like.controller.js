2ac1519e5687d830e0316e1dc8ae3fc3fb59cad3
Aarun1349 <arunmahendra90@gmail.com>
2024-02-15 14:21:05 +0530

    Complete like controller and update like model

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,109 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/likes.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/likes.model.js";
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import {
+  asyncHandler,
+  asyncHandlerPromiseVersion,
+} from "../utils/asyncHandler.js";
+import { Comment } from "../models/comments.model.js";
+import { Tweet } from "../models/tweets.model.js";
 
-const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+const toggleVideoLike = asyncHandlerPromiseVersion(async (req, res) => {
+  //TODO: toggle like on video
+
+  const { videoId } = req.params;
+  const userId = req.user.id;
+  if (!userId) {
+    throw new ApiError(400, "please login first");
+  }
+  const getVideoById = await Video.findById(videoId);
+  if (!getVideoById) {
+    throw new ApiError(404, "Requested video not found");
+  }
+
+  const toggleLike = await Like.create({
+    video: videoId,
+    likedBy: userId,
+    isLiked: !isLiked,
+  });
+
+  if (!toggleLike) {
+    throw new ApiError(400, "Can't like this");
+  }
+
+  res.status(200).json(new ApiResponse(200, toggleLike, "success"));
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  //TODO: toggle like on comment
 
-})
+  const { commentId } = req.params;
+
+  const userId = req.user.id;
+  if (!userId) {
+    throw new ApiError(400, "please login first");
+  }
+  const getCommentsById = await Comment.findById(commentId);
+  if (!getCommentsById) {
+    throw new ApiError(404, "Requested comment not found");
+  }
+
+  const toggleLike = await Like.create({
+    comment: commentId,
+    likedBy: userId,
+    isLiked: !isLiked,
+  });
+
+  if (!toggleLike) {
+    throw new ApiError(400, "Can't like this");
+  }
+
+  res.status(200).json(new ApiResponse(200, toggleLike, "success"));
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  //TODO: toggle like on tweet
+  const { tweetId } = req.params;
+
+  const userId = req.user.id;
+  if (!userId) {
+    throw new ApiError(400, "please login first");
+  }
+  const getTweetById = await Tweet.findById(tweetId);
+  if (!getTweetById) {
+    throw new ApiError(404, "Requested tweet not found");
+  }
+
+  const toggleLike = await Like.create({
+    tweet: tweetId,
+    likedBy: userId,
+    isLiked: !isLiked,
+  });
+
+  if (!toggleLike) {
+    throw new ApiError(400, "Can't like this");
+  }
+
+  res.status(200).json(new ApiResponse(200, toggleLike, "success"));
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+  //TODO: get all liked videos
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+  const userId = req.user.id;
+  if (!userId) {
+    throw new ApiError(400, "please login first");
+  }
+  const getLikedVideoByUser = await await Like.find({
+    likedBy: userId,
+  });
+  if (!getLikedVideoByUser) {
+    throw new ApiError(404, "Requested comment not found");
+  }
+
+  res.status(200).json(new ApiResponse(200, getLikedVideoByUser, "success"));
+});
+
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
